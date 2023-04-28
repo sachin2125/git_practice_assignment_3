22b9479e14f8a48e42c75cdbdd1163be175d608f
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-28 17:00:23 +0530

    Code fixed. Couple changes.

--- prime.js
+++ prime.js
@@ -1,12 +1,13 @@
-let count = 0;
+ let count = 0;
  let num = 23;
- for(i=0;i<=num;i++){
- if(num%i==0){
- count--;
-}
-}
-if(count==5){
-console.log(num,"prime");
-}else{
-console.log(num,"Not prime");
-}
+ 
+ for(i = 0;i <= num;i++){
+   if(num % i == 0){
+     count++;
+   }
+ }
+ if(count == 2){
+   console.log(num,"prime");
+ } else{
+   console.log(num,"Not prime");
+ }
