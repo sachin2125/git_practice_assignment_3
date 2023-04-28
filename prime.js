c9b2c64ca892ed6d2e189bce1966f2de6be78f3b
unknown <maliksachin298@gmail.com>
2023-04-28 16:58:09 +0530

    changes made by me

--- prime.js
+++ prime.js
@@ -2,10 +2,10 @@ let count = 0;
  let num = 23;
  for(i=0;i<=num;i++){
  if(num%i==0){
- count--;
+ count++;
 }
 }
-if(count==5){
+if(count==2){
 console.log(num,"prime");
 }else{
 console.log(num,"Not prime");
